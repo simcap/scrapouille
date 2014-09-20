1fbd387912631c1e8c9f97498b8abafe570f6bb2
simcap <simon@preplaysports.com>
2014-09-20 17:22:24 +0200

    only one rule instance variable

--- lib/scrapouille.rb
+++ lib/scrapouille.rb
@@ -4,31 +4,30 @@ require 'nokogiri'
 class Scrapouille
 
   def initialize(&block)
-    @rules = []
-    @collection_rules = []
+    @rules = {single: [], multiple: []} 
     instance_eval(&block) if block_given?
   end
 
   def scrap_all(property, xpath_options)
     raise "Missing 'at:' option for '#{property}'" unless xpath_options[:at]
     xpath_string = xpath_options[:at]
-    @collection_rules << [property, xpath_string]
+    @rules[:multiple] << [property, xpath_string]
   end
 
   def scrap(property, xpath_options)
     raise "Missing 'at:' option for '#{property}'" unless xpath_options[:at]
     xpath_string = xpath_options[:at]
     if block_given?
-      @rules << [property, xpath_string, Proc.new]
+      @rules[:single] << [property, xpath_string, Proc.new]
     else
-      @rules << [property, xpath_string]
+      @rules[:single] << [property, xpath_string]
     end
   end
 
   def scrap!(uri)
-    web_page = open(uri).read
-    html = Nokogiri::HTML(web_page)
-    item_results = @rules.inject({}) do |result, rule|
+    page = open(uri).read
+    html = Nokogiri::HTML(page)
+    item_results = @rules[:single].inject({}) do |result, rule|
       property, xpath, block = rule
 
       content = html.xpath(xpath)
@@ -44,7 +43,7 @@ class Scrapouille
       result
     end
 
-    collection_results = @collection_rules.inject({}) do |result, rule|
+    collection_results = @rules[:multiple].inject({}) do |result, rule|
       property, xpath, block = rule
       content = html.xpath(xpath)
       if peek = content.first
